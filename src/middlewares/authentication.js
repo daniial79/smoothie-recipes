1adae8a64f6552729731ba1a769321d803101669
danial <dpezeshpoor@yahoo.com>
2022-05-07 18:29:25 +0430

    authentication logic has been completed

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,27 @@
+//third-party packages and libs
+const e = require('express');
+const jwt = require('jsonwebtoken');
+
+//User models
+const User = require('../models/User');
+
+//logic section 
+const authenticate =  (req, res, next) => {
+    const token = req.cookies.authToken;
+
+    if(token){
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        if(decodedToken){
+            next();
+        }else{
+            res.redirect('/login');
+        }
+    }else{
+        console.log(decodedToken);
+        res.redirect('/login');
+    }
+}
+
+
+//exporting section 
+module.exports = authenticate;
--- src/routes/mainPages.routes.js
+++ src/routes/mainPages.routes.js
@@ -4,11 +4,14 @@ const router = require('express').Router();
 //importing main pages controllers
 const mainPagesControllers= require('../controllers/mainPages.controllers');
 
+//importing authentication middleware
+const authenticate = require('../middlewares/authentication');
+
 //rendering home page
 router.get('/', mainPagesControllers.renderHomePage);
 
 //rendering smoothie-recipes page
-router.get('/smoothies', mainPagesControllers.renderSmoothiePage);
+router.get('/smoothies', authenticate , mainPagesControllers.renderSmoothiePage);
 
 //exporting section 
 module.exports = router;
