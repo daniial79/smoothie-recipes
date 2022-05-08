92bfa1d1a67d3f67cdf95548812e04edac4bfb44
danial <dpezeshpoor@yahoo.com>
2022-05-08 13:29:59 +0430

    some enhancements

--- src/server.js
+++ src/server.js
@@ -6,12 +6,11 @@ const server = require('./app');
 
 //database and server configs
 const port = process.env.PORT;
-const hostName = process.env.HOST_NAME;
 const dataBaseUri = process.env.DB_URI;
 
 //database connection and server listener 
 mongoose.connect(dataBaseUri, { 
     useNewUrlParser: true
 })
-  .then((result) => server.listen(port, hostName, () => console.log(`server is up and running at http://${hostName}:${port}`)))
+  .then((result) => server.listen(port, hostName, () => console.log(`server is up and running at port ${port}`)))
   .catch((err) => console.log(err));
